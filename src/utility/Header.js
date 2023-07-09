3fc468ebf9edad0de42ed53c1870266aff78615e
Kashish Gandhi <kashishgandhi6112003@gmail.com>
2023-07-09 13:14:41 +0530

    header update

--- src/utility/Header.js
+++ src/utility/Header.js
@@ -1,51 +1,135 @@
 import "../css/Header.css";
+import { useLocation } from 'react-router-dom'
 
 const Header = () => {
+  const location = useLocation();
+  const currentPage = location.pathname;
   return (
     <div className="header_main">
       <div className="header_title">
         <b className="header_title_container">
           <span className="header_title_container1">
-            <span>{`Find your `}</span>
-            <span className="dream_course_container">dream course</span>
-          </span>
-        </b>
+          {currentPage === '/Courses' ? (
+         <span>{`Find your `}</span>
+      ) : currentPage === '/applyresearch' ? (
+        <span>{`Write your `}</span>
+      ) : currentPage === '/applyresearch' ? (
+        <span>{`Find your `}</span>
+      ) : null}
+{currentPage === '/Courses' ? (
+         <span className="dream_course_container">dream course</span>
+      ) : currentPage === '/applyresearch' ? (
+        <span className="dream_course_container">research paper</span>
+      ) : currentPage === '/applyresearch' ? (
+        <span className="dream_course_container">dream internship</span>
+      ) : null}
+
+</span>
+
+</b>
+            {/* <span>{`Find your `}</span> */}
+            {/* <span className="dream_course_container">dream course</span> */}
+      
         <img className="header_underline_course" alt="" src="/image-7@2x.png" />
       </div>
-      <div className="header_enhance_skills_container">
+      {currentPage === '/Courses' ? (
+         <div className="header_enhance_skills_container">
+         Enhance your skills by learning courses from the faculty in top
+         universities
+       </div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_enhance_skills_container">
+        Write your research paper under the best professors and get their LORs
+      </div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_enhance_skills_container">
+        Find your next career at companies like Jio, KPMG, and Intel
+      </div>
+      ) : null}
+      
+
+      {/* <div className="header_enhance_skills_container">
         Enhance your skills by learning courses from the faculty in top
         universities
-      </div>
+      </div> */}
       <section className="header_rectangle_parent">
         <div className="header_frame_child" />
         <div className="header_frame_item" />
         <div className="header_frame_group">
           <img className="header_search_icon_frame" alt="" src="/frame.svg" />
-          <div className="header_course_name_placeholder">Course Name or keyword</div>
+
+          {currentPage === '/Courses' ? (
+       <div className="header_course_name_placeholder">Course Name or keyword</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder">Domain</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder">Role Name or keyword</div>
+      ) : null}
+
+          {/* <div className="header_course_name_placeholder">Course Name or keyword</div> */}
           <div className="header_frame_inner" />
           <div className="header_line_div" />
           <div className="header_frame_child1" />
         </div>
         <div className="header_frame_container">
           <img className="header_search_icon_frame" alt="" src="/frame.svg" />
-          <div className="header_course_name_placeholder1">Course Name or keyword</div>
+          {currentPage === '/Courses' ? (
+       <div className="header_course_name_placeholder1">Course Name or keyword</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder1">Domain</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder1">Role Name or keyword</div>
+      ) : null}
+          {/* <div className="header_course_name_placeholder1">Course Name or keyword</div> */}
           <div className="header_frame_child2" />
           <div className="header_line_div" />
           <div className="header_frame_child1" />
         </div>
         <div className="header_frame_div">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_university_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_university_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_university_placeholder">Company Name or Professor Name</div>
+      ) : null}
+{/* 
+          <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child5" />
         </div>
         <div className="header_main1">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_university_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_university_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_university_placeholder">Company Name or Professor Name</div>
+      ) : null}
+          {/* <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child6" />
         </div>
         <div className="header_main2">
           <img className="header_search_icon_frame4" alt="" src="/frame1.svg" />
-          <div className="header_search">University</div>
+
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_search">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_search">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_search">Company Name or Professor Name</div>
+      ) : null}
+          {/* <div className="header_search">University</div> */}
           <div className="header_frame_child7" />
         </div>
         <div className="header_main3">
@@ -66,31 +150,84 @@ const Header = () => {
         <div className="header_frame_item" />
         <div className="header_frame_group">
           <img className="header_search_icon_frame" alt="" src="/frame.svg" />
-          <div className="header_course_name_placeholder">Course Name or keyword</div>
+          {currentPage === '/Courses' ? (
+       <div className="header_course_name_placeholder">Course Name or keyword</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder">Domain</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="header_course_name_placeholder">Role Name or keyword</div>
+      ) : null}
+          {/* <div className="header_course_name_placeholder">Course Name or keyword</div> */}
           <div className="header_frame_inner" />
           <div className="header_line_div" />
           <div className="header_frame_child1" />
         </div>
         <div className="header_frame_container">
+
           <img className="header_search_icon_frame" alt="" src="/frame.svg" />
-          <div className="header_course_name_placeholder1">University</div>
+
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_course_name_placeholder1">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_course_name_placeholder1">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_course_name_placeholder1">Company Name or Professor Name</div>
+      ) : null}
+
+          {/* <div className="header_course_name_placeholder1">University</div> */}
           <div className="header_frame_child2" />
           <div className="header_line_div" />
           <div className="header_frame_child1" />
         </div>
         <div className="header_frame_div">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_course_name_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_course_name_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_course_name_placeholder">Company Name or Professor Name</div>
+      ) : null}
+
+
+          {/* <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child5" />
         </div>
         <div className="header_main1">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_course_name_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_course_name_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_course_name_placeholder">Company Name or Professor Name</div>
+      ) : null}
+
+          {/* <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child6" />
         </div>
         <div className="header_main2">
           <img className="header_search_icon_frame4" alt="" src="/frame1.svg" />
-          <div className="header_search">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_search">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_search">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_search">Company Name or Professor Name</div>
+      ) : null}
+          {/* <div className="header_search">University</div> */}
           <div className="header_frame_child7" />
         </div>
         <div className="header_main3">
@@ -111,7 +248,15 @@ const Header = () => {
         <div className="header_frame_item" />
         <div className="header_frame_group">
           <img className="header_search_icon_frame16" alt="" src="/frame.svg" />
-          <div className="search6">Course Name or keyword</div>
+
+          {currentPage === '/Courses' ? (
+       <div className="search6">Course Name or keyword</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="search6">Domain</div>
+      ) : currentPage === '/applyresearch' ? (
+        <div className="search6">Role Name or keyword</div>
+      ) : null}
+          {/* <div className="search6">Course Name or keyword</div> */}
           <div className="header_frame_inner" />
           <div className="header_line_div" />
           <div className="header_frame_child1" />
@@ -125,12 +270,34 @@ const Header = () => {
         </div>
         <div className="header_frame_div">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_course_name_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_course_name_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_course_name_placeholder">Company Name or Professor Name</div>
+      ) : null}
+
+          {/* <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child5" />
         </div>
         <div className="header_main1">
           <img className="header_search_icon_frame2" alt="" src="/frame1.svg" />
-          <div className="header_university_placeholder">University</div>
+          {currentPage === '/Courses' ? (
+      
+      <div className="header_course_name_placeholder">University</div>
+      ) : currentPage === '/applyresearch' ? (
+       
+        <div className="header_course_name_placeholder">Professor Name</div>
+      ) : currentPage === '/applyresearch' ? (
+    
+        <div className="header_course_name_placeholder">Company Name or Professor Name</div>
+      ) : null}
+
+          {/* <div className="header_university_placeholder">University</div> */}
           <div className="header_frame_child6" />
         </div>
         <div className="header_main2">
