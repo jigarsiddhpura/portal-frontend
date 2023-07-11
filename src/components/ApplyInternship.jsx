e69252159e1e1a38bedfb4b9af64005df630f762
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2023-07-11 17:50:31 +0530

    integrated internship + pagination done

--- src/components/ApplyInternship.jsx
+++ src/components/ApplyInternship.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Typography, Chip, Stack, Button } from "@mui/material";
 import ResponsiveDrawer from "../utility/ResponsiveDrawer";
 import Avatar from "@mui/material/Avatar";
@@ -9,9 +8,45 @@ import ScheduleIcon from "@mui/icons-material/Schedule";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 
 import gojo from "../images/gojo.png";
+import Pagination from "@mui/material/Pagination";
 
 const ApplyInternship = () => {
   // checking local Storage after each update
+
+  const [internships, setInternships] = useState([]);
+
+  const getInternships = () => {
+    try {
+      var myHeaders = new Headers();
+
+      myHeaders.append(
+        "Authorization",
+        "Token 74153f1697d48cb9e8614bb564b7cf2e161d5aa7"
+      );
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://ipbackend.pythonanywhere.com/internship-list/",
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((result) => setInternships(JSON.parse(result)))
+        .catch((error) => console.log("error", error));
+    } catch (err) {
+      console.log("Error while fetching", err);
+    }
+  };
+
+  useEffect(() => {
+    getInternships();
+  }, []);
+
+  // console.log(internships);
   var [isDrawerOpen, setIsDrawerOpen] = useState(true);
 
   window.onstorage = () => {
@@ -29,98 +64,172 @@ const ApplyInternship = () => {
     },
   });
 
-  const ResearchCard = () => {
+  const InternshipCard = ({ items }) => {
     return (
       <ThemeProvider theme={theme}>
-        <Grid
-          container
-          elevation={2}
-          style={{
-            border: "1px solid gray",
-            borderRadius: "5px",
-            width: "50rem",
-            padding: "1rem",
-            margin: "2rem 0 0 5rem",
-          }}
-        >
-          <Grid
-            item
-            xs={12}
-            sm={3}
-            md={2}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Avatar
-              alt="Remy Sharp"
-              src={gojo}
-              sx={{ width: 56, height: 56 }}
-            />
-          </Grid>
-          <Grid item md={4}>
-            <Typography variant="h5" gutterBottom align="left">
-              Frontend Development
-            </Typography>
-            <Typography variant="body2" gutterBottom align="left">
-              Sridhar Iyer, Remote
-            </Typography>
-            <Stack direction="row" spacing={1} style={{ marginTop: "0.5rem" }}>
-              <Chip label="FULL TIME" color="primary" variant="outlined" />
-              <Chip label="React" color="primary" />
-              <Chip label="MUI" color="primary" />
-            </Stack>
-          </Grid>
-          <Grid item md={3} paddingLeft={3}>
-            <Stack direction="column" spacing={2}>
-              <Stack direction="row" spacing={2}>
-                <CalendarMonthIcon color="success" />{" "}
-                <span style={{ color: "black", marginBottom: "0.3rem" }}>
-                  {" "}
-                  20hrs/week{" "}
-                </span>
-              </Stack>
-              <Stack direction="row" spacing={2}>
-                <ScheduleIcon color="success" />{" "}
-                <span style={{ color: "black", marginBottom: "0.3rem" }}>
-                  {" "}
-                  20hrs/week{" "}
-                </span>
-              </Stack>
-              <Stack direction="row" spacing={2}>
-                <CurrencyRupeeIcon color="success" />{" "}
-                <span style={{ color: "black", marginBottom: "0.3rem" }}>
-                  {" "}
-                  5000{" "}
-                </span>
-              </Stack>
-            </Stack>
-          </Grid>
-          <Grid
-            item
-            md={3}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Button variant="contained" color="secondary">
-              Apply
-            </Button>
-          </Grid>
+        <Grid md={12}>
+          {internships.map((internship, index) => {
+            var skills = internship.skills.split(",");
+            skills = skills.filter((element) => element !== "");
+            if (items.includes(internship.id)) {
+              return (
+                <Grid
+                  key={index}
+                  container
+                  elevation={2}
+                  style={{
+                    border: "1px solid gray",
+                    borderRadius: "5px",
+                    width: "50rem",
+                    padding: "1rem",
+                    margin: "2rem 0 0 5rem",
+                  }}
+                >
+                  <Grid
+                    item
+                    xs={12}
+                    sm={3}
+                    md={2}
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Avatar
+                      alt="Remy Sharp"
+                      src={gojo}
+                      sx={{ width: 56, height: 56 }}
+                    />
+                  </Grid>
+                  <Grid item md={4}>
+                    <Typography variant="h5" gutterBottom align="left">
+                      {internship.internship_Title}
+                    </Typography>
+                    <Typography variant="body2" gutterBottom align="left">
+                      Sridhar Iyer, {internship.internship_Type}
+                    </Typography>
+                    <Stack
+                      direction="row"
+                      spacing={1}
+                      style={{
+                        marginTop: "0.5rem",
+                        width: "16rem",
+                        // border: "2px solid red",
+                        flexWrap: "wrap",
+                      }}
+                    >
+                      <Chip
+                        label={internship.internship_Type}
+                        color="primary"
+                        variant="outlined"
+                        style={{ marginBottom: "5px" }}
+                      />
+                      {skills.map((skill) => (
+                        <Chip
+                          key={skill}
+                          label={skill.trim()}
+                          color="primary"
+                        />
+                      ))}
+                    </Stack>
+                  </Grid>
+                  <Grid item md={3} paddingLeft={3}>
+                    <Stack direction="column" spacing={2}>
+                      <Stack direction="row" spacing={2}>
+                        <CalendarMonthIcon color="success" />
+                        <span
+                          style={{ color: "black", marginBottom: "0.3rem" }}
+                        >
+                          {internship.start_date} to <br />
+                          {internship.end_date}
+                        </span>
+                      </Stack>
+                      <Stack direction="row" spacing={2}>
+                        <ScheduleIcon color="success" />{" "}
+                        <span
+                          style={{ color: "black", marginBottom: "0.3rem" }}
+                        >
+                          {internship.organization}
+                        </span>
+                      </Stack>
+                      <Stack direction="row" spacing={2}>
+                        <CurrencyRupeeIcon color="success" />{" "}
+                        <span
+                          style={{ color: "black", marginBottom: "0.3rem" }}
+                        >
+                          {internship.stipend}
+                        </span>
+                      </Stack>
+                    </Stack>
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button variant="contained" color="secondary">
+                      Apply
+                    </Button>
+                  </Grid>
+                </Grid>
+              );
+            }
+            return null;
+          })}
         </Grid>
       </ThemeProvider>
     );
   };
 
-  const AllResearchCards = () => {
+  const AllInternshipCards = ({ itemsPerPage }) => {
+    // implemented pagination functionality
+
+    const items = Array.from(
+      { length: internships.length },
+      (_, index) => index + 1
+    );
+
+    const [itemOffset, setItemOffset] = useState(0);
+
+    const endOffset = itemOffset + itemsPerPage;
+
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
+    const currentItems = items.slice(itemOffset, endOffset);
+    console.log(currentItems);
+    const pageCount = Math.ceil(items.length / itemsPerPage);
+
+    const [page, setPage] = useState(1);
+
+    // Invoke when user click to request another page.
+    const handleChange = (event, page) => {
+      const index = page - 1;
+      const newOffset = (index * itemsPerPage) % items.length;
+      setItemOffset(newOffset);
+      const currentItems = items.slice(itemOffset, endOffset);
+      setPage(page);
+    };
+
     return (
-      <React.Fragment>
-        <ResearchCard />
-      </React.Fragment>
+      <div style={{ display: "flex", alignItems: "center" , flexDirection:'column'}} >
+        <InternshipCard items={currentItems} />
+
+        <Stack spacing={2} style={{ display: "flex", alignItems: "center" }}>
+          <Pagination
+            className="pagination"
+            shape="rounded"
+            count={pageCount}
+            color="success"
+            page={page}
+            onChange={handleChange}
+          />
+        </Stack>
+      </div>
     );
   };
 
@@ -129,10 +238,10 @@ const ApplyInternship = () => {
       <div>
         <ResponsiveDrawer drawerHeading={"All Internships"} />
         <div
-          className="researchContainer"
+          className="InternshipContainer"
           style={{ marginLeft: isDrawerOpen ? "245px" : "10px" }}
         >
-          <AllResearchCards />
+          <AllInternshipCards itemsPerPage={4} />
         </div>
       </div>
     </>
--- src/components/PostInternship.jsx
+++ src/components/PostInternship.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -9,38 +10,105 @@ import * as yup from "yup";
 import humanAI from "../images/humanAI.png";
 import "../css/InternshipForm.css";
 import makeAnimated from "react-select/animated";
+import { type } from "@testing-library/user-event/dist/type";
 import { skillsOptions } from "../docs/data.ts";
 import Select from "react-select";
 
 const validationSchema = yup.object({
   position: yup.string("ML Engineer").required("Position is required"),
-  remote: yup.string("Remote").required("Required Field"),
-  duration: yup.string("1 month").required("Required Field"),
-  positionsOpen: yup.string("1").required("Position is required"),
-  stipend: yup.string("3000").required("Stipend is required"),
-  skillsRequired: yup.string("React.js").required("Required Field"),
+  internship_type: yup
+    .string("internship_type")
+    .required("Internship type is required"),
+  start_date: yup
+    .date()
+    .min(new Date(), "Selected date must be in future")
+    .required("Start date is required"),
+  end_date: yup
+    .date()
+    .min(new Date(), "Selected date must be in future")
+    .required("End date is required"),
+  stipend: yup.number(1000).required("Stipend is required"),
+  eligibility: yup.string("B.Tech").required("Eligibility is required"),
+  organization: yup.string("B.Tech").required("Organization is required"),
+  location: yup.string("Mumbai"),
+  positionsOpen: yup.number(1).required("Number of positions is required"),
+  skillsRequired: yup.array().required("Required Field"),
 });
 
 const PostInternshipForm = () => {
   const formik = useFormik({
     initialValues: {
       position: "",
-      remote: "",
-      duration: "",
+      internship_type: "",
       positionsOpen: "",
       stipend: "",
-      skillsRequired: "",
+      skillsRequired: [],
+      start_date: "",
+      end_date: "",
+      eligibility: "",
+      organization: "",
+      location: "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
+      selectedValues.forEach((item, index) => {
+        selectedValues[index] = item.value;
+      });
+      var concat_values = selectedValues.join(",");
+      formik.values.skillsRequired = concat_values;
       alert(JSON.stringify(values, null, 2));
+      sendData(values);
     },
   });
 
+  const sendData = (values) => {
+    try {
+      var myHeaders = new Headers();
+
+      myHeaders.append(
+        "Authorization",
+        "Token 74153f1697d48cb9e8614bb564b7cf2e161d5aa7"
+      );
+
+      // myHeaders.append("Cookie", "csrftoken=o9U6wKWbEVIt5Ha31j7UIfXxtowMJPR6");
+
+      console.log(myHeaders);
+
+      var formdata = new FormData();
+      formdata.append("skills", values.skillsRequired);
+      formdata.append("internship_Title", values.position);
+      formdata.append("internship_Type", values.internship_type);
+      formdata.append("start_date", values.start_date);
+      formdata.append("end_date", values.end_date);
+      formdata.append("stipend", values.stipend);
+      formdata.append("eligibility", values.eligibility);
+      formdata.append("organization", values.organization);
+      formdata.append("location", values.location);
+      formdata.append("no_Of_Openings", values.positionsOpen);
+
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: formdata,
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://ipbackend.pythonanywhere.com/internship-create/",
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) =>
+          console.log("Error while posting internship : ", error)
+        );
+    } catch (err) {
+      throw new Error("Invalid data");
+    }
+  };
+
   // adding event listener for responsiveness
   const [width, setWindowWidth] = useState(0);
-
-  const color = "#4A2145";
 
   useEffect(() => {
     updateDimensions();
@@ -59,7 +127,7 @@ const PostInternshipForm = () => {
   const responsiveness2 = { responsive: width < 850 };
   const resp2 = responsiveness2.responsive;
 
-  const Post = styled(Button)({
+  const PostBtn = styled(Button)({
     backgroundColor: "#85D1A0",
     margin: "1rem",
     borderRadius: "0.3rem",
@@ -96,6 +164,248 @@ const PostInternshipForm = () => {
     }),
   };
 
+  const [focus_startDate, setFocusedStartDate] = useState(false);
+  const onFocusStartDate = () => setFocusedStartDate(true);
+
+  const [focus_endDate, setFocusedendDate] = useState(false);
+  const onFocusendDate = () => setFocusedendDate(true);
+
+  const StartDateField = () => {
+    return (
+      <>
+        <TextField
+          id="start_date"
+          type={focus_startDate ? "date" : "text"}
+          label="start Date"
+          placeholder="start Date"
+          onFocus={onFocusStartDate}
+          name="start_date"
+          value={formik.values.start_date}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.touched.start_date && Boolean(formik.errors.start_date)}
+          helperText={formik.touched.start_date && formik.errors.start_date}
+          required
+          style={{ margin: "0 1rem 1rem 0", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const EndDateField = () => {
+    return (
+      <>
+        <TextField
+          id="end_date"
+          type={focus_endDate ? "date" : "text"}
+          label="End Date"
+          placeholder="End Date"
+          onFocus={onFocusendDate}
+          // onBlur={onBlur}
+          name="end_date"
+          value={formik.values.end_date}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.touched.end_date && Boolean(formik.errors.end_date)}
+          helperText={formik.touched.end_date && formik.errors.end_date}
+          required
+          style={{ marginBottom: "1rem", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const OrganizationField = () => {
+    return (
+      <>
+        <TextField
+          id="organization"
+          type="text"
+          label="Organization"
+          placeholder="Organization"
+          name="organization"
+          value={formik.values.organization}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.organization && Boolean(formik.errors.organization)
+          }
+          helperText={formik.touched.organization && formik.errors.organization}
+          required
+          style={{margin: "0 1rem 1rem 0", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const LocationField = () => {
+    return (
+      <>
+        <TextField
+          id="location"
+          type="text"
+          label="Location"
+          placeholder="Location"
+          name="location"
+          value={formik.values.location}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.location && Boolean(formik.errors.location)
+          }
+          helperText={formik.touched.location && formik.errors.location}
+          required
+          style={{ marginBottom: "1rem", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const PositonField = () => {
+    return (
+      <>
+        <TextField
+          id="position"
+          type="text"
+          label="Position"
+          placeholder="Position"
+          name="position"
+          value={formik.values.position}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.touched.position && Boolean(formik.errors.position)}
+          helperText={formik.touched.position && formik.errors.position}
+          required
+          style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
+        />
+      </>
+    );
+  };
+
+  const InternshipTypeField = () => {
+    return (
+      <>
+        <TextField
+          id="internship_type"
+          type="text"
+          label="Internship Type"
+          placeholder="Remote/Onsite"
+          name="internship_type"
+          value={formik.values.internship_type}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.internship_type &&
+            Boolean(formik.errors.internship_type)
+          }
+          helperText={
+            formik.touched.internship_type && formik.errors.internship_type
+          }
+          required
+          style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
+        />
+      </>
+    );
+  };
+
+  const PositionsOpenField = () => {
+    return (
+      <>
+        <TextField
+          id="positionsOpen"
+          type="number"
+          label="Positions Open"
+          placeholder="Positions Open"
+          name="positionsOpen"
+          value={formik.values.positionsOpen}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.positionsOpen && Boolean(formik.errors.positionsOpen)
+          }
+          helperText={
+            formik.touched.positionsOpen && formik.errors.positionsOpen
+          }
+          required
+          style={{ margin: "0 1rem 1rem 0", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const StipendField = () => {
+    return (
+      <>
+        <TextField
+          id="stipend"
+          type="number"
+          label="Stipend"
+          placeholder="Stipend"
+          name="stipend"
+          value={formik.values.stipend}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.touched.stipend && Boolean(formik.errors.stipend)}
+          helperText={formik.touched.stipend && formik.errors.stipend}
+          required
+          style={{ marginBottom: "1rem", width: resp ? "20ch" : "24ch" }}
+        />
+      </>
+    );
+  };
+
+  const EligibilityField = () => {
+    return (
+      <>
+        <TextField
+          id="eligibility"
+          type="text"
+          label="Eligibility"
+          placeholder="Eligibility"
+          name="eligibility"
+          value={formik.values.eligibility}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.eligibility && Boolean(formik.errors.eligibility)
+          }
+          helperText={formik.touched.eligibility && formik.errors.eligibility}
+          required
+          style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch"}}
+        />
+      </>
+    );
+  };
+
+  const SkillsField = () => {
+    return (
+      <>
+        <Select
+          required
+          className="skillsRequired"
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          // defaultValue={[skillsOptions[4], skillsOptions[5]]}
+          isMulti
+          options={skillsOptions}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary25: "#b2ebf2",
+              primary: "black",
+            },
+          })}
+          value={formik.values.selectedValues}
+          styles={selectStyles}
+          onChange={handleSelectChange}
+          placeholder="Required skills"
+        />
+      </>
+    );
+  };
+
   return (
     <Grid
       container
@@ -110,126 +420,51 @@ const PostInternshipForm = () => {
     >
       <Grid
         item
-        xs={12} md={6}
+        xs={12}
+        md={6}
         style={{
           display: "flex",
           justifyContent: "center",
-          alignItems: "center"
+          alignItems: "center",
         }}
       >
         <form onSubmit={formik.handleSubmit}>
           <Box
             sx={{
-              // maxWidth: "31rem",
-              // width: "31rem",
-              // height: "38rem",
-              // border: "2px solid black",
               display: "flex",
               flexDirection: "column",
               alignItems: resp ? "center" : "flex-start",
               padding: "1rem",
               position: "relative",
-              // left:'4rem',
             }}
           >
             <h1 className="formHeader">Post Internship</h1>
 
-            <TextField
-              id="position"
-              type="text"
-              label="Position"
-              placeholder="Position"
-              name="position"
-              onChange={formik.handleChange}
-              error={formik.touched.position && Boolean(formik.errors.position)}
-              helperText={formik.touched.position && formik.errors.position}
-              required
-              style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
-            />
+            {PositonField()}
 
-            <TextField
-              id="remote"
-              type="text"
-              label="Remote"
-              placeholder="Remote/Onsite"
-              name="remote"
-              onChange={formik.handleChange}
-              error={formik.touched.remote && Boolean(formik.errors.remote)}
-              helperText={formik.touched.remote && formik.errors.remote}
-              required
-              style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
-            />
+            {InternshipTypeField()}
 
-            <TextField
-              id="duration"
-              type="number"
-              label="Duration (in months)"
-              placeholder="Duration"
-              name="duration"
-              onChange={formik.handleChange}
-              error={formik.touched.duration && Boolean(formik.errors.duration)}
-              helperText={formik.touched.duration && formik.errors.duration}
-              required
-              style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
-            />
+            <div style={{ display: "flex" }}>
+              {StartDateField()}
+              {EndDateField()}
+            </div>
 
-            <TextField
-              id="positionsOpen"
-              type="number"
-              label="Positions Open"
-              placeholder="Positions Open"
-              name="positionsOpen"
-              onChange={formik.handleChange}
-              error={
-                formik.touched.positionsOpen &&
-                Boolean(formik.errors.positionsOpen)
-              }
-              helperText={
-                formik.touched.positionsOpen && formik.errors.positionsOpen
-              }
-              required
-              style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
-            />
+            <div style={{ display: "flex" }}>
+              {OrganizationField()}
+              {LocationField()}
+            </div>
 
-            <TextField
-              id="stipend"
-              type="number"
-              label="Stipend"
-              placeholder="Stipend"
-              name="stipend"
-              onChange={formik.handleChange}
-              error={formik.touched.stipend && Boolean(formik.errors.stipend)}
-              helperText={formik.touched.stipend && formik.errors.stipend}
-              required
-              style={{ marginBottom: "1rem", width: resp ? "42ch" : "50ch" }}
-            />
+            <div style={{ display: "flex" }}> 
+              {PositionsOpenField()}
+              {StipendField()}
+            </div>
 
-            <Select
-              required
-              className="skillsRequired"
-              closeMenuOnSelect={false}
-              components={animatedComponents}
-              // defaultValue={[skillsOptions[4], skillsOptions[5]]}
-              isMulti
-              options={skillsOptions}
-              theme={(theme) => ({
-                ...theme,
-                borderRadius: 0,
-                colors: {
-                  ...theme.colors,
-                  primary25: "#b2ebf2",
-                  primary: "black",
-                },
-              })}
-              styles={selectStyles}
-              onChange={handleSelectChange}
-              placeholder="Required skills"
-            />
+            {EligibilityField()}
 
-            <div
-            // style={{ display: "flex" }}
-            >
-              <Post type="submit">Post Internship</Post>
+            {SkillsField()}
+
+            <div>
+              <PostBtn type="submit">Post Internship</PostBtn>
             </div>
           </Box>
         </form>
